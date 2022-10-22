26b0b7c6cad641717dc6222bd24107ad83664d19
arel hagag <areltv297@gmail.com>
2022-10-23 01:02:48 +0300

    fdf

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const gameBoard = document.getElementById("game-board");
 const tools = document.querySelector(".tools");
-let groundCounter = document.querySelector('.ground-counter');
-let grassCounter = document.querySelector('.grass-counter');
-let stoneCounter = document.querySelector('.stone-counter');
-let woodCounter = document.querySelector('.wood-counter');
-let treeCounter = document.querySelector('.tree-counter');
-let leafCounter = document.querySelector('.leaf-counter');
-const axe = document.querySelector('.axe');
-const pickaxe = document.querySelector('.pickaxe');
-const shovel = document.querySelector('.shovel');
+let groundCounter = document.querySelector(".soil-counter");
+let grassCounter = document.querySelector(".grass-counter");
+let stoneCounter = document.querySelector(".rock-counter");
+let woodCounter = document.querySelector(".leaves-counter");
+let treeCounter = document.querySelector(".tree-counter");
+let leafCounter = document.querySelector(".leaf-counter");
+const axe = document.querySelector(".axe");
+const pickaxe = document.querySelector(".pickaxe");
+const shovel = document.querySelector(".shovel");
 
-axe.addEventListener('click', e => {
-    currentTool=axe;
-    axe.style.border = '1px solid red'
-    pickaxe.style.border = '1px solid white'
-    shovel.style.border = '1px solid white'
-})
-pickaxe.addEventListener('click', e => {
-    currentTool=pickaxe;
-    axe.style.border = '1px solid white'
-    pickaxe.style.border = '1px solid red'
-    shovel.style.border = '1px solid white'
-})
-shovel.addEventListener('click', e => {
-    currentTool=shovel;
-    axe.style.border = '1px solid white'
-    pickaxe.style.border = '1px solid white'
-    shovel.style.border = '1px solid red'
-})
-
+axe.addEventListener("click", (e) => {
+  currentTool = axe;
+  axe.style.border = "1px solid red";
+  pickaxe.style.border = "1px solid white";
+  shovel.style.border = "1px solid white";
+});
+pickaxe.addEventListener("click", (e) => {
+  currentTool = pickaxe;
+  axe.style.border = "1px solid white";
+  pickaxe.style.border = "1px solid red";
+  shovel.style.border = "1px solid white";
+});
+shovel.addEventListener("click", (e) => {
+  currentTool = shovel;
+  axe.style.border = "1px solid white";
+  pickaxe.style.border = "1px solid white";
+  shovel.style.border = "1px solid red";
+});
 
 let myObject = { gridWidth: 20, gridHight: 15 };
+
 createWorld();
 function createWorld() {
   for (let i = 0; i < myObject.gridWidth; i++) {
@@ -43,49 +43,73 @@ function createWorld() {
   }
 }
 
-const divs= document.querySelectorAll("cell")
-divs.forEach(div=>div.addEventlistener("click",console.log(div)))
-
-for(let i=0;i<gameBoard.children.length;i++){
-    
-    let [a,b]=gameBoard.children[i].getAttribute('id').slice(5).split("-")
-    //['0','0']
-    if(a>14){
-        gameBoard.children[i].classList.add("soil")
-    }
-    if(a>13&&a<15){
-        gameBoard.children[i].classList.add("grass")
-    }
-    if(a>12&&a<14){
-        gameBoard.children[i].classList.add("leaf")
-    }
-    if(a<13&&a>2&b>13){
-        gameBoard.children[i].classList.add("rock")
-    }
-    if(a<13&&a>2&&b<1){
-        gameBoard.children[i].classList.add("rock")
-    }
-    if(a>1&&a<3){
-        gameBoard.children[i].classList.add("rock")
-    }
-    if(a>8&&a>2&&b>6&&b<9){
-        gameBoard.children[i].classList.add("tree")
-    }
-    if(a<9&&a>6&b>4&&b<11){
-        gameBoard.children[i].classList.add("leaves")
-    }
+for (let i = 0; i < gameBoard.children.length; i++) {
+  let [a, b] = gameBoard.children[i].getAttribute("id").slice(5).split("-");
+  //['0','0']
+  if (a > 14) {
+    gameBoard.children[i].classList.add("soil");
+  }
+  if (a > 13 && a < 15) {
+    gameBoard.children[i].classList.add("grass");
+  }
+  if (a > 12 && a < 14) {
+    gameBoard.children[i].classList.add("leaf");
+  }
+  if (a < 13 && (a > 2) & (b > 13)) {
+    gameBoard.children[i].classList.add("rock");
+  }
+  if (a < 13 && a > 2 && b < 1) {
+    gameBoard.children[i].classList.add("rock");
+  }
+  if (a > 1 && a < 3) {
+    gameBoard.children[i].classList.add("rock");
+  }
+  if (a > 8 && a > 2 && b > 6 && b < 9) {
+    gameBoard.children[i].classList.add("tree");
+  }
+  if (a < 9 && (a > 6) & (b > 4) && b < 11) {
+    gameBoard.children[i].classList.add("leaves");
+  }
 }
 
+gameBoard.addEventListener("click", () => {
+  console.log(event.target.getAttribute("class"));
+  if(event.target.getAttribute("class").includes("tree")){
+    event.target.classList.remove("tree")
 
+  }
+});
 
-
-
-
-
-
-
-
-
+gameBoard.addEventListener("click", () => {
+    console.log(event.target.getAttribute("class"));
+    if(event.target.getAttribute("class").includes("rock")){
+      event.target.classList.remove("rock")
+    }
+  });
+  gameBoard.addEventListener("click", () => {
+    console.log(event.target.getAttribute("class"));
+    if(event.target.getAttribute("class").includes("soil")){
+      event.target.classList.remove("soil")
+    }
+  });
+  gameBoard.addEventListener("click", () => {
+    console.log(event.target.getAttribute("class"));
+    if(event.target.getAttribute("class").includes("leaf")){
+      event.target.classList.remove("leaf")
+    }
+  });
+  gameBoard.addEventListener("click", () => {
+    console.log(event.target.getAttribute("class"));
+    if(event.target.getAttribute("class").includes("leaves")){
+      event.target.classList.remove("leaves")
+    }
+  });
+  gameBoard.addEventListener("click", () => {
+    console.log(event.target.getAttribute("class"));
+    if(event.target.getAttribute("class").includes("grass")){
+      event.target.classList.remove("grass")
+    }
+  });
 
 //  function fill(){
 //     for(let k = 0;k< gameBoard.children.length;k++){
